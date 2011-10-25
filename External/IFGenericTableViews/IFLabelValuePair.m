8534f20ae3529ffdc7081b906d521ce2d8172150
Gi Lee <glee@ziaconsulting.com>
2011-10-24 18:26:02 -0600

    Added MPL 1.1 Licenses as appropriate, removed classes/frameworks which are not used by the project

--- External/IFGenericTableViews/IFLabelValuePair.m
+++ External/IFGenericTableViews/IFLabelValuePair.m
@@ -1,9 +1,27 @@
 //
-//  IFKeyValuePair.m
-//  XpenserUtility
+//  ***** BEGIN LICENSE BLOCK *****
+//  Version: MPL 1.1
 //
-//  Created by Bindu Wavell on 1/10/10.
-//  Copyright 2010 Zia Consulting, Inc.. All rights reserved.
+//  The contents of this file are subject to the Mozilla Public License Version
+//  1.1 (the "License"); you may not use this file except in compliance with
+//  the License. You may obtain a copy of the License at
+//  http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS" basis,
+//  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+//  for the specific language governing rights and limitations under the
+//  License.
+//
+//  The Original Code is the Alfresco Mobile App.
+//  The Initial Developer of the Original Code is Zia Consulting, Inc.
+//  Portions created by the Initial Developer are Copyright (C) 2011
+//  the Initial Developer. All Rights Reserved.
+//
+//
+//  ***** END LICENSE BLOCK *****
+//
+//
+//  IFKeyValuePair.m
 //
 
 #import "IFLabelValuePair.h"
