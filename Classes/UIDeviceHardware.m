574f18ebc6ef085207ff9307267798c9f31f3f90
rvillarreal <rvillarreal@ziaconsulting.com>
2012-04-04 13:21:46 -0500

    FDIOS-301: Updated the UIDeviceHardware with the latest iPhone 4s and iPad 3 models

--- Classes/UIDeviceHardware.m
+++ Classes/UIDeviceHardware.m
@@ -49,6 +49,7 @@
     if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
     if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
     if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
+    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
     if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
     if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
     if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
@@ -57,7 +58,11 @@
     if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
     if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
     if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
+    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad-3G (WiFi)";
+    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad-3G (4G)";
+    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad-3G (4G)";
     if ([platform isEqualToString:@"i386"])         return @"Simulator";
+    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
     return platform;
 }
 
