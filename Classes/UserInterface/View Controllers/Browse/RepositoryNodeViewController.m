b42fe9a8e53a531e624ea34159bcb9220e8c596a
Mohamad Saeedi <mohamad.saeedi@alfresco.com>
2012-06-27 12:15:12 +0100

    Resolved bug MOBILE-381, Available in build 1.3.0.3, rotating ipad does not close the photo/video popup and the screen is not cropped

--- Classes/UserInterface/View Controllers/Browse/RepositoryNodeViewController.m
+++ Classes/UserInterface/View Controllers/Browse/RepositoryNodeViewController.m
@@ -247,7 +247,7 @@ NSString * const kMultiSelectDelete = @"deleteAction";
 {
     [super viewWillDisappear:animated];
     
-    [self.popover dismissPopoverAnimated:YES];
+    //[self.popover dismissPopoverAnimated:YES];
     if (self.actionSheet.window)
     {
         [self.actionSheet dismissWithClickedButtonIndex:self.actionSheet.cancelButtonIndex animated:animated];
@@ -705,6 +705,8 @@ NSString * const kMultiSelectDelete = @"deleteAction";
                 
                 [picker release];
             }
+            
+            [[UIApplication sharedApplication] setStatusBarHidden:NO];
             
 		}
         else if ([buttonLabel isEqualToString:NSLocalizedString(@"add.actionsheet.create-folder", @"Create Folder")]) 
