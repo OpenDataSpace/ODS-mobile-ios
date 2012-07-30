21825726853962e282e7ed60c79e6bea046753c6
rvillarreal <rvillarreal@ziaconsulting.com>
2012-07-30 16:38:47 -0500

    FDIOS-548: Replaced the AccountStatus with an AccountStatus object. The account status information it's separate from the account information to  minimize serialization/deserialization of the AccountInfo objects in the KeyChain

--- Classes/UserInterface/View Controllers/Accounts/Row Render/AccountRowRender.m
+++ Classes/UserInterface/View Controllers/Accounts/Row Render/AccountRowRender.m
@@ -55,13 +55,10 @@
         [accountCell setTag:index];
         [accountCell.textLabel setText:[detail description]];
         
-        if([detail accountStatus] == FDAccountStatusAwaitingVerification)
+        NSString *statusShortMessage = [[detail accountStatusInfo] shortMessage];
+        if(statusShortMessage)
         {
-            [accountCell.detailTextLabel setText:NSLocalizedString(@"account.awaiting.cell.subtitle", @"Awaiting Email Verification")];
-        }
-        if([detail accountStatus] == FDAccountStatusInactive)
-        {
-            [accountCell.detailTextLabel setText:NSLocalizedString(@"account.inactive.cell.subtitle", @"Inactive account subtitle")];
+            [accountCell.detailTextLabel setText:statusShortMessage];
         }
         [[accountCell imageView]setImage:[UIImage imageNamed:iconImageName]];
         
