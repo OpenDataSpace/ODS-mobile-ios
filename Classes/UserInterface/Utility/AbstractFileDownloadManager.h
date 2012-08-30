9907cc01ed610f07347a7b3ed88c4312c3b37c44
Mohamad Saeedi <mohamad.saeedi@alfresco.com>
2012-08-30 14:41:13 +0100

    index on topic-MOBILE-Sync: 42d4ad7 Mobile Sync: refactored FileUtils

---
+++ Classes/UserInterface/Utility/AbstractFileDownloadManager.h
@@ -0,0 +1,13 @@
+//
+//  AbstractFileDownloadManager.h
+//  FreshDocs
+//
+//  Created by Mohamad Saeedi on 30/08/2012.
+//  Copyright (c) 2012 U001b. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AbstractFileDownloadManager : NSObject
+
+@end
---
+++ Classes/UserInterface/Utility/AbstractFileDownloadManager.m
@@ -0,0 +1,13 @@
+//
+//  AbstractFileDownloadManager.m
+//  FreshDocs
+//
+//  Created by Mohamad Saeedi on 30/08/2012.
+//  Copyright (c) 2012 U001b. All rights reserved.
+//
+
+#import "AbstractFileDownloadManager.h"
+
+@implementation AbstractFileDownloadManager
+
+@end
