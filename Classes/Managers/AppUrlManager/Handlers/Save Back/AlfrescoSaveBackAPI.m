e28979a152b4d1ba4581dcfb0d320fbe545db579
Mike Hatfield <mikehatfield@gmail.com>
2012-08-30 14:02:11 +0100

    MOBILE-299, MOBILE-538-541: Added SaveBack API documentation

--- Classes/Managers/AppUrlManager/Handlers/Save Back/AlfrescoSaveBackAPI.m
+++ Classes/Managers/AppUrlManager/Handlers/Save Back/AlfrescoSaveBackAPI.m
@@ -21,15 +21,29 @@
 
 #import "AlfrescoSaveBackAPI.h"
 
+/**
+ * AlfrescoBundleIdentifier can be matched against application in documentInteractionController:willBeginSendingToApplication:
+ */
 NSString * const AlfrescoBundleIdentifier = @"com.alfresco.mobile";
+
+/**
+ * The Alfresco private metadata will be stored in the annotation dictionary under this key. It should be returned in the
+ * same format, using the same key, when invoking a Save Back operation.
+ */
 NSString * const AlfrescoSaveBackMetadataKey = @"AlfrescoMetadata";
+
+/**
+ * To limit the "Open In..." list of available apps during a Save Back operation, the document must be renamed so that
+ * the following extension is appended to the filename. For example "My Document.docx.alf01"
+ */
 NSString * const AlfrescoSaveBackDocumentExtension = @".alf01";
+
 
 /**
  * Save Back to Alfresco helper function.
  *
- * We need to copy (move/rename would also be ok depending on the application) the current file
- * to one with the AlfrescoSaveBackDocumentExtension
+ * The function is given the path to a document and will return a URL representing a suitably renamed document
+ * ready to participate in the Save Back operation. The renamed document will be created in the temporary directory.
  */
 NSURL *alfrescoSaveBackURLForFilePath(NSString *filePath, NSError **error)
 {
@@ -39,9 +53,16 @@ NSURL *alfrescoSaveBackURLForFilePath(NSString *filePath, NSError **error)
     NSFileManager *fileManager = [NSFileManager defaultManager];
     if ([fileManager fileExistsAtPath:tempSaveBackPath])
     {
-        [fileManager removeItemAtPath:tempSaveBackPath error:error];
+        if ([fileManager removeItemAtPath:tempSaveBackPath error:error] == NO)
+        {
+            return nil;
+        }
     }
-    [fileManager copyItemAtPath:filePath toPath:tempSaveBackPath error:error];
+    
+    if ([fileManager copyItemAtPath:filePath toPath:tempSaveBackPath error:error] == NO)
+    {
+        return nil;
+    }
     
     return [NSURL fileURLWithPath:tempSaveBackPath];
 }
