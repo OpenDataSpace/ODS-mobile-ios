78c44129c6a1bac1291cdf10428cdb10f59da1df
tijsrademakers <tijs.rademakers@alfresco.com>
2012-09-17 11:38:59 +0200

    Added filter icon

--- Classes/UserInterface/View Controllers/Tasks/TasksTableViewController.m
+++ Classes/UserInterface/View Controllers/Tasks/TasksTableViewController.m
@@ -127,7 +127,7 @@ static NSString *FilterTasksStartedByMe = @"filter_startedbymetasks";
     [Theme setThemeForUINavigationBar:self.navigationController.navigationBar];
     
     [self.navigationItem setTitle:NSLocalizedString(@"tasks.view.mytasks.title", nil)];
-    self.navigationItem.leftBarButtonItem = [[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Tasks_button"] style:UIBarButtonItemStyleBordered 
+    self.navigationItem.leftBarButtonItem = [[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Alfresco_iOS_Filter.png"] style:UIBarButtonItemStyleBordered 
                                                                                                  target:self action:@selector(filterTasksAction:)] autorelease];
     self.navigationItem.rightBarButtonItem = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
                                                                               target:self action:@selector(addTaskAction:)] autorelease];
