52e199d5db9b64d7a01592a40ede519ef1a5e2ca
Mohamad Saeedi <mohamad.saeedi@alfresco.com>
2013-02-22 14:53:05 +0000

    MOBILE-1127: bland thumbnail is displayed when deleting synced file from share and preforming save back

--- Classes/Managers/Favorite/FavoriteManager.m
+++ Classes/Managers/Favorite/FavoriteManager.m
@@ -306,7 +306,7 @@ NSString * const kDocumentsToBeDeletedLocallyAfterUpload = @"toBeDeletedLocallyA
             {
                 RepositoryItem *repoItem = [searchedDocument objectAtIndex:0];
                 
-                if ([self findNodeInFavorites:repoItem.guid] == nil)
+                if (repoItem.title != nil && ![repoItem.title isEqualToString:@""] && [self findNodeInFavorites:repoItem.guid] == nil)
                 {
                     FavoriteTableCellWrapper *cellWrapper = [[FavoriteTableCellWrapper alloc] initWithRepositoryItem:repoItem];
                     cellWrapper.accountUUID = [(CMISQueryHTTPRequest *)request accountUUID];
@@ -334,12 +334,15 @@ NSString * const kDocumentsToBeDeletedLocallyAfterUpload = @"toBeDeletedLocallyA
             
             for (RepositoryItem *repoItem in searchedDocuments)
             {
+                if(repoItem.title != nil && ![repoItem.title isEqualToString:@""])
+                {
                 FavoriteTableCellWrapper *cellWrapper = [[[FavoriteTableCellWrapper alloc] initWithRepositoryItem:repoItem] autorelease];
                 
                 cellWrapper.accountUUID = [(CMISQueryHTTPRequest *)request accountUUID];
                 cellWrapper.tenantID = [(CMISQueryHTTPRequest *)request tenantID];
                 
                 [self.favorites addObject:cellWrapper];
+                }
             }
         }
         
@@ -573,8 +576,9 @@ NSString * const kDocumentsToBeDeletedLocallyAfterUpload = @"toBeDeletedLocallyA
                 
                 NSDictionary *fileDownloadinfo = [fileManager downloadInfoForFilename:[fileURL lastPathComponent]];
                 NSString *accountUUID = [fileDownloadinfo objectForKey:@"accountUUID"];
+                NSString *docTitle = [fileDownloadinfo objectForKey:@"filename"];
                 
-                if ([activeAccountUuids containsObject:accountUUID])
+                if (docTitle != nil && ![docTitle isEqualToString:@""] && [activeAccountUuids containsObject:accountUUID])
                 {
                     RepositoryItem *item = [[RepositoryItem alloc] initWithDictionary:fileDownloadinfo];
                     FavoriteTableCellWrapper *cellWrapper = [[FavoriteTableCellWrapper alloc] initWithRepositoryItem:item];
