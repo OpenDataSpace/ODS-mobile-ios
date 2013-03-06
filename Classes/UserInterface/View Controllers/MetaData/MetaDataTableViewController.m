a83f65941a59a65da7e83340a3c0c6a488776c85
Mohamad Saeedi <mohamad.saeedi@alfresco.com>
2013-03-06 15:26:51 +0000

    MOBILE-1399 	The display of the Offline Expiry Timeout field needs improving

--- Classes/UserInterface/View Controllers/MetaData/MetaDataTableViewController.m
+++ Classes/UserInterface/View Controllers/MetaData/MetaDataTableViewController.m
@@ -254,9 +254,9 @@ static NSArray * cmisPropertiesToDisplay = nil;
             
             if ([key hasPrefix:@"dp:offlineExpiresAfter"])
             {
-                // converting the expiry time to hours from milliseconds
-                double intervalInHours = [[model objectForKey:key] doubleValue] / 1000.0 / 60 / 60;
-                NSString *valueString = [NSString stringWithFormat:@"%.3f", intervalInHours];
+                // converting the expiry time to hours from milliseconds and rounding to nearest integer
+                long intervalInHours = (long)round([[model objectForKey:key] doubleValue] / 1000.0 / 60 / 60);
+                NSString *valueString = [NSString stringWithFormat:@"%lu", intervalInHours];
                 key = [key stringByAppendingString:@"Ex"];
                 [model setObject:valueString forKey:key];
             }
